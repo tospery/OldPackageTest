44eed60dcbb4e10d7d238255a5cd74ad67add4b4
tospery <tospery@gmail.com>
2024-03-16 05:11:43 +0800

    替换rpc的传输协议为http

---
+++ http_rpc_test/client/client.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+import requests
+
+request = {
+	"id": 0,
+	"params": ["bobby"],
+	"method": "HelloService.Hello"
+}
+
+rsp = requests.post("http://localhost:1234/jsonrpc", json=request)
+print(rsp.text)
+*/
